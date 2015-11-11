b7d51320b77820b61060e185d09824264a49dd2c
Nathan Farrar <nathan.farrar@gmail.com>
2015-11-10 19:39:59 -0700

    added sh config

---
+++ .config/sh/profile.d/00-xdg.sh
@@ -0,0 +1,6 @@
+# Author: Nathan Farrar <nfarrar@crunk.io>
+# Description: xdg exports
+
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
